6aa230acb0cd0720ebb7597304085b6577d12663
zaraazzar <azzar.arza18@gmail.com>
2023-11-05 19:33:12 +0700

    Init Lazy Layout Course

---
+++ JetHeroes/app/src/main/java/com/dicoding/jetheroes/model/HeroesData.kt
@@ -0,0 +1,256 @@
+package com.dicoding.jetheroes.model
+
+object HeroesData {
+    val heroes = listOf(
+        Hero(
+            "1",
+            "Abdul Muis",
+            "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/1.jpg"
+        ),
+        Hero(
+            "2",
+            "Ki Hajar Dewantara",
+            "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/2.jpg"
+        ),
+        Hero(
+            "3",
+            "R.M. Surjopranoto",
+            "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/3.jpg"
+        ),
+        Hero(
+            "4",
+            "Mohammad Hoesni Thamrin",
+            "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/4.jpg"
+        ),
+        Hero(
+            "5",
+            "KH. Samanhudi",
+            "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/5.jpg"
+        ),
+        Hero(
+            "6",
+            "H.O.S. Cokroaminoto",
+            "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/6.jpg"
+        ),
+        Hero(
+            "7",
+            "Danudirja Setiabudi",
+            "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/7.jpg"
+        ),
+        Hero(
+            "8",
+            "Si Singamangaradja XII",
+            "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/8.jpg"
+        ),
+        Hero(
+            "9",
+            "Dr. G.S.S.J Ratulangie",
+            "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/9.jpg"
+        ),
+        Hero(
+            "10",
+            "DR. Sutomo",
+            "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/10.jpg"
+        ),
+        Hero(
+            "11",
+            "KH. Akhmad Dahlan",
+            "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/11.jpg"
+        ),
+        Hero(
+            "12",
+            "KH. Agus Salim",
+            "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/12.jpg"
+        ),
+        Hero(
+            "13",
+            "Jend. Gatot Subroto",
+            "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/13.jpg"
+        ),
+        Hero(
+            "14",
+            "Sukarjo Wiryopranoto",
+            "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/14.jpg"
+        ),
+        Hero(
+            "15",
+            "Dr. Ferdinand Lumban Tobing",
+            "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/15.jpg"
+        ),
+        Hero(
+            "16",
+            "KH. Zainul Arifin",
+            "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/16.jpg"
+        ),
+        Hero(
+            "17",
+            "Tan Malaka",
+            "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/17.jpg"
+        ),
+        Hero(
+            "18",
+            "MGR.A. Sugyopranoto SJ",
+            "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/18.jpg"
+        ),
+        Hero(
+            "19",
+            "Ir. H. Djuanda Kartawijaya",
+            "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/19.jpg"
+        ),
+        Hero(
+            "20",
+            "DR. Sahardjo, SH",
+            "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/20.jpg"
+        ),
+        Hero(
+            "21",
+            "Cut Nyak Dhien",
+            "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/21.jpg"
+        ),
+        Hero(
+            "22",
+            "Cut Meutia",
+            "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/22.jpg"
+        ),
+        Hero(
+            "23",
+            "R.A. Kartini",
+            "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/23.jpg"
+        ),
+        Hero(
+            "24",
+            "Dr. Ciptomangunkusumo",
+            "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/24.jpg"
+        ),
+        Hero(
+            "25",
+            "H. Fakhrudin",
+            "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/25.jpg"
+        ),
+        Hero(
+            "26",
+            "KH. Mas Mansyur",
+            "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/26.jpg"
+        ),
+        Hero(
+            "27",
+            "Alimin",
+            "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/27.jpg"
+        ),
+        Hero(
+            "28",
+            "Dr. Muwardi",
+            "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/28.jpg"
+        ),
+        Hero(
+            "29",
+            "KH. Abdul Wahid Hasjim",
+            "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/29.jpg"
+        ),
+        Hero(
+            "30",
+            "Sri Susuhunan Pakubuwono VI",
+            "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/30.jpg"
+        ),
+        Hero(
+            "31",
+            "KH. Hasjim Asjarie",
+            "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/31.jpg"
+        ),
+        Hero(
+            "32",
+            "Gubernur Surjo",
+            "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/32.jpg"
+        ),
+        Hero(
+            "33",
+            "Jenderal Soedirman",
+            "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/33.jpg"
+        ),
+        Hero(
+            "34",
+            "Jenderal Urip Sumohardjo",
+            "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/34.jpg"
+        ),
+        Hero(
+            "35",
+            "Prof. DR. Supomo, SH",
+            "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/35.jpg"
+        ),
+        Hero(
+            "36",
+            "DR. Kusumaatmadja, SH",
+            "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/36.jpg"
+        ),
+        Hero(
+            "37",
+            "Jenderal TNI Anm. A. Yani",
+            "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/37.jpg"
+        ),
+        Hero(
+            "38",
+            "Letjen. TNI.R. Anm Suprapto",
+            "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/38.jpg"
+        ),
+        Hero(
+            "39",
+            "Letjen. TNI. Anm. MT. Harjono",
+            "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/39.jpg"
+        ),
+        Hero(
+            "40",
+            "Letjen. TNI. Anm. S. Parman",
+            "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/40.jpg"
+        ),
+        Hero(
+            "41",
+            "Mayjen. TNI. Anm. D.I. Panjaitan",
+            "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/41.jpg"
+        ),
+        Hero(
+            "42",
+            "Mayjen. TNI.Anm. S. Siswomihardjo",
+            "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/42.jpg"
+        ),
+        Hero(
+            "43",
+            "Kapten CZI. Anm. Pierre Tendean",
+            "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/43.jpg"
+        ),
+        Hero(
+            "44",
+            "AIP. TK. II Brig.Pol. K.S. Tubun",
+            "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/44.jpg"
+        ),
+        Hero(
+            "45",
+            "Brigjen. TNI. Anm. Katamso",
+            "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/45.jpg"
+        ),
+        Hero(
+            "46",
+            "Kol. Inf. TNI. Anm. Sugiono",
+            "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/46.jpg"
+        ),
+        Hero(
+            "47",
+            "Sutan Sjahrir",
+            "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/47.jpg"
+        ),
+        Hero(
+            "48",
+            "Laks.Laut R.E. Martadinata",
+            "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/48.jpg"
+        ),
+        Hero(
+            "49",
+            "Raden Dewi Sartika",
+            "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/49.jpg"
+        ),
+        Hero(
+            "50",
+            "Prof. DR. W.Z. Johannes",
+            "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/50.jpg"
+        )
+    )
+}
